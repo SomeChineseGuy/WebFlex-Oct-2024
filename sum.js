67ad7a1405ca21299c8014931ea3334232368998
Alvin Ng <alvin.cl.ng@gmail.com>
2024-10-23 17:42:47 -0700

    Added the slice and have a working array

---
+++ sum.js
@@ -0,0 +1,36 @@
+// The function will take in any number of numbers from our CML and add all the numbers up together. We will not take negivate numbers
+
+// to read our command line arguments
+
+// console.log(process.argv);
+
+// let myName = "Alvin"
+// myName = "Joe!"
+
+// console.log(myName);
+
+// const arr = [1,2,3]
+
+// arr.push(10)
+// console.log(arr);
+// arr = {
+//   a: "Alvin"
+// }
+
+// what is the difference between let and var
+// console.log(myName);
+// let myName = "Alvin"
+// console.log(myName2);
+// var myName2 = "Steve"
+
+// if(true) {
+//   var num = 10;
+// }
+
+// console.log(num)
+
+const args = process.argv;
+
+console.log(args.slice(2));
+
+const sliced = args.slice(2);
